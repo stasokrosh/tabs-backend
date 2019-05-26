b6912c8dbbee3afb0f4e630918f7961a5a1ca0bd
stasokrosh <stasokrosh@yandex.com>
2019-05-26 22:18:28 +0300

    remote mongo connection

--- src/util/db-config.js
+++ src/util/db-config.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 //MongoDB connection
-let dev_db_url = 'mongodb://localhost:27017/tabs';
+let dev_db_url = 'mongodb://stasokrosh:1812tqqs@mongodb-2798-0.cloudclusters.net:10011/tabs?authSource=admin';
 let mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
